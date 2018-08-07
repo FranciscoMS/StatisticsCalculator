aaa500ed1264a6a09b9e367fa896cef6f45e5383
Francisco Méndez Soto <francisco.mendez@avantica.net>
2018-08-07 16:56:22 -0600

    Models and DBContext

---
+++ BE/StatisticsCalculator/StatisticsCalculator.Data/Models/Document.cs
@@ -0,0 +1,21 @@
+﻿using StatisticsCalculator.Data.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace StatisticsCalculator.Data.Models
+{
+    public class Document
+    {
+        [Key]
+        [Column(Order = 1)]
+        public Guid DocumentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public virtual User User{ get; set; }
+    }
+}
---
+++ BE/StatisticsCalculator/StatisticsCalculator.Data/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StatisticsCalculator.Data.Models
+{
+    public class User
+    {
+        [Key]
+        [Column(Order = 1)]
+        public Guid UserID { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(8)]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
